be9959afe20f8aecdc89cdf41b283c60c065dc2b
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2020-12-13 19:34:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase()+string.splice(1)
+  }
+  static capitalize(string){
+    return string.charAt(0).toUpperCase()+string.splice(1)
+  }
 }
