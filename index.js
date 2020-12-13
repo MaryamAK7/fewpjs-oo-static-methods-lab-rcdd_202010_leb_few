222269107f8bd0a8796be8555c99eeeea679935d
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2020-12-13 19:43:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class Formatter {
   //add static methods here
   static capitalize(string){
-    return string.charAt(0).toUpperCase()+string.splice(1)
+    return string.charAt(0).toUpperCase()+string.slice(1)
   }
   static sanitize(string){
     return string.replace( /[^A-Za-z0-9 '-]/g, '' )
